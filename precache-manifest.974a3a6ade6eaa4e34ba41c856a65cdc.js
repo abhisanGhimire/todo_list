75ae8beab3cbb1ab104d450a9e5cd187071424b1
abhisanGhimire <abhisanghimire@dr-ait.org>
2020-09-05 23:47:14 +0545

    Updates

---
+++ precache-manifest.974a3a6ade6eaa4e34ba41c856a65cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a1e606a2491f70c7e2ece03d2bd646",
+    "url": "/abhisanGhimire/todo_list/index.html"
+  },
+  {
+    "revision": "9a1e4dfeea3eb26be088",
+    "url": "/abhisanGhimire/todo_list/static/css/main.cbb37d64.chunk.css"
+  },
+  {
+    "revision": "23f59fef570c3f7566c8",
+    "url": "/abhisanGhimire/todo_list/static/js/2.42570932.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abhisanGhimire/todo_list/static/js/2.42570932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1e4dfeea3eb26be088",
+    "url": "/abhisanGhimire/todo_list/static/js/main.6fc6f2f5.chunk.js"
+  },
+  {
+    "revision": "bf1fe23dfd6c4bba0996",
+    "url": "/abhisanGhimire/todo_list/static/js/runtime-main.1ae58693.js"
+  }
+]);
